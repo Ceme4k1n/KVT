5e715fe4e0cb7ada3e4a2034a5777412c412dfbe
Звягин Егор Александрович <zviagin@bolid.ru>
2025-03-27 14:14:42 +0300

    Fix: logs

--- src/server.js
+++ src/server.js
@@ -104,3 +104,5 @@ process.on('SIGINT', () => {
     process.exit()
   })
 })
+
+//Данные с датчиков {"0":{"humidity":34,"id":1,"status":165,"temperature":24,"timestamp":"2025-03-27T11:11:39.600Z"},"1":{"humidity":34,"id":2,"status":165,"temperature":24,"timestamp":"2025-03-27T11:11:40.048Z"},"2":{"humidity":36.0625,"id":3,"status":165,"temperature":24.8125,"timestamp":"2025-03-27T11:11:40.107Z"},"3":{"humidity":35,"id":4,"status":165,"temperature":24,"timestamp":"2025-03-27T11:11:40.699Z"},"4":{"humidity":35,"id":5,"status":76,"temperature":24,"timestamp":"2025-03-27T11:11:41.189Z"},"5":{"humidity":35,"id":6,"status":71,"temperature":24,"timestamp":"2025-03-27T11:11:41.637Z"},"6":{"humidity":35,"id":7,"status":165,"temperature":24,"timestamp":"2025-03-27T11:11:42.247Z"},"7":{"humidity":35,"id":8,"status":165,"temperature":24,"timestamp":"2025-03-27T11:11:42.779Z"},"8":{"humidity":35,"id":9,"status":165,"temperature":24,"timestamp":"2025-03-27T11:11:43.309Z"},"9":{"humidity":34,"id":10,"status":165,"temperature":24,"timestamp":"2025-03-27T11:11:43.960Z"},"timestamp":"2025-03-27T11:11:43.962Z"}
